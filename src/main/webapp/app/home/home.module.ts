645df43c07c672c44fbd3593b26b89087f6df208
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-17 13:12:27 +0200

    [FORGE_DEV] YC-PROTAS-S1-003-FE: src/main/webapp/app/home/home.module.ts

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,12 +4,11 @@ import { RouterModule } from '@angular/router';
 import SharedModule from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import SharedModule from 'app/shared/shared.module';
-import { HOME_ROUTE } from './home.route';
+// DashboardComponent est un standalone component et ne doit pas être déclaré ici
+// Il sera importé directement dans HomeComponent si nécessaire.
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent, DashboardComponent],
+  declarations: [HomeComponent],
 })
 export class ProtaskkillaaaHomeModule {}
