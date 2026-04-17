5e504c716fba4104b7f3f1dce05fd6a772a8921b
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-17 16:15:34 +0200

    [FORGE_DEV] Fix attempt 2: src/main/webapp/app/home/home.module.ts

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { TaskDashboardModule } from 'app/entities/task/task-dashboard/task-dashboard.module'; // <-- Add this import
+// Remplacer l'importation du module par l'importation directe du composant standalone
+import { DashboardComponent } from 'app/entities/task/task-dashboard/dashboard.component'; // Supposons que c'est un composant standalone
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), TaskDashboardModule], // <-- Add TaskDashboardModule here
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), DashboardComponent], // Ajouter DashboardComponent directement comme standalone
   declarations: [HomeComponent],
 })
 export class HomeModule {}
