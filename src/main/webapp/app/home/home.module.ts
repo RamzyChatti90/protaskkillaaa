49a7d4921f98083ab646c55abff1871862da2547
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-17 14:45:42 +0200

    [YC-PROTAS-S1-003-FE] home.module.ts

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import SharedModule from 'app/shared/shared.module';
+import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskDashboardModule } from 'app/entities/task/task-dashboard/task-dashboard.module'; // <-- Add this import
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), DashboardComponent],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), TaskDashboardModule], // <-- Add TaskDashboardModule here
   declarations: [HomeComponent],
 })
-export class ProtaskkillaaaHomeModule {}
+export class HomeModule {}
