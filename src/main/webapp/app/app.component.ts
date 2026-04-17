36c8d3cc47a8e1c6aadd8f54e523d40c18f6f90b
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-17 03:48:13 +0200

    [FORGE_DEV] Fix attempt 6: src/main/webapp/app/app.component.ts

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import dayjs from 'dayjs/esm';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
@@ -7,6 +7,7 @@ import locale from '@angular/common/locales/en';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { AccountService } from 'app/core/auth/account.service';
 import { fontAwesomeIcons } from './config/font-awesome-icons';
 import MainComponent from './layouts/main/main.component';
 
@@ -18,15 +19,21 @@ import MainComponent from './layouts/main/main.component';
     // jhipster-needle-angular-add-module JHipster will add new module here
   ],
 })
-export default class AppComponent {
+export default class AppComponent implements OnInit {
   private readonly applicationConfigService = inject(ApplicationConfigService);
   private readonly iconLibrary = inject(FaIconLibrary);
   private readonly dpConfig = inject(NgbDatepickerConfig);
+  private readonly accountService = inject(AccountService); // Inject AccountService
 
   constructor() {
     this.applicationConfigService.setEndpointPrefix(SERVER_API_URL);
     registerLocaleData(locale);
     this.iconLibrary.addIcons(...fontAwesomeIcons);
     this.dpConfig.minDate = { year: dayjs().subtract(100, 'year').year(), month: 1, day: 1 };
+  }
+
+  ngOnInit(): void {
+    // Force user identity loading at application startup to react to a potentially existing JWT token in localStorage
+    this.accountService.authenticate(true);
   }
 }
