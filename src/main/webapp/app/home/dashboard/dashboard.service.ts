9600c8b042bfb10bf021e5ea9693e4f569522408
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-17 15:19:31 +0200

    [FORGE_DEV] YC-PROTAS-S1-003-FE: src/main/webapp/app/home/dashboard/dashboard.service.ts

--- src/main/webapp/app/home/dashboard/dashboard.service.ts
+++ src/main/webapp/app/home/dashboard/dashboard.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
 import { DashboardData } from './dashboard.model';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
-  protected resourceUrl = this.applicationConfigService.getEndpointFor('api/dashboard');
+  private readonly applicationConfigService = inject(ApplicationConfigService);
+  private readonly http = inject(HttpClient);
 
-  constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
+  private readonly resourceUrl = this.applicationConfigService.getEndpointFor('api/dashboard');
 
   getDashboardData(): Observable<DashboardData> {
     return this.http.get<DashboardData>(this.resourceUrl);
